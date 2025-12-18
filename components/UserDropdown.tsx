928725fe1c09ae3eb6417af6d178c2e0cc6b6e9d
samir-45 <mdmahinkhan621@gmail.com>
2025-12-18 11:58:25 +0600

    Implement header

---
+++ components/UserDropdown.tsx
@@ -0,0 +1,71 @@
+"use client"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { useRouter } from "next/navigation";
+import { Button } from "./ui/button";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { LogOut } from "lucide-react";
+import NavItems from "./NavItems";
+// import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime";
+
+const UserDropdown = () => {
+
+  const router = useRouter();
+
+  const handleSignOut = async () => {
+    router.push('/sign-in');
+  }
+
+  const user = { name: "samir", email: "mdmahinkhan621@gmail.com" }
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" className="flex items-center gap-3 text-gray-400 hover:text-yellow-500">
+          <Avatar className="h-8 w-8">
+            <AvatarImage src="https://github.com/shadcn.png" />
+            <AvatarFallback className="bg-yellow-500 text-yellow-900 text-sm font-bold">
+              {user.name[0].toLocaleUpperCase()}
+            </AvatarFallback>
+          </Avatar>
+          <div className="hidden md:flex flex-col items-start">
+            <span className="text-base font-medium text-gray-400">{user.name}</span>
+          </div>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="text-gray-400">
+        <DropdownMenuLabel>
+          <div className="flex relative items-center gap-3 py-2">
+            <Avatar className="h-10 w-10">
+              <AvatarImage src="https://github.com/shadcn.png" />
+              <AvatarFallback className="bg-yellow-500 text-yellow-900 text-sm font-bold">
+                {user.name[0].toLocaleUpperCase()}
+              </AvatarFallback>
+            </Avatar>
+            <div className="flex flex-col">
+              <span className="text-base font-medium text-gray-400">{user.name}</span>
+              <span className="text-sm text-gray-500">{user.email}</span>
+            </div>
+          </div>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator className="bg-gray-600"></DropdownMenuSeparator>
+        <DropdownMenuItem onClick={handleSignOut} className="text-gray-100 text-md font-medium focus:bg-transparent focus:text-yellow-500 transition-colors cursor-pointer">
+          <LogOut className="mr-2 h-4 w-4 hidden sm:block" />
+          Logout
+        </DropdownMenuItem>
+        <DropdownMenuSeparator className="bg-gray-600 hidden sm:block"></DropdownMenuSeparator>
+        <nav className="sm:hidden">
+          <NavItems></NavItems>
+        </nav>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
+
+export default UserDropdown
